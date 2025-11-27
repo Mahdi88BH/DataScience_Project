5d78a9613c825b2582dc3123a7a35b80aa7fb1f5
Elmahdi Bahadia <elmahdibahadia@gmail.com>
2025-11-27 05:40:28 -0500

    Data Validation Pipeline

--- README.md
+++ README.md
@@ -8,9 +8,9 @@
 5. Model Evaluation 
 
 
-### Steps of Data Ingestion :
-1. Update config.yaml
-2. Update Schema.yaml
+### WorFlows Steps :
+1. Update config.yaml (all_process)
+2. Update Schema.yaml (validation_data)
 3. Update params.yaml
 4. Update the entity
 5. Update the configuration manager in src config
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.Wine_Quality_Prediction import logger
 from src.Wine_Quality_Prediction.pipeline.data_Ingestion_pipeline import DataIngestionTrainingPipeline
+from src.Wine_Quality_Prediction.pipeline.data_Validation_pipeline import DataValidationTrainingPipeline
 
 logger.info("Welcome to our project Data Science Prediction to the Wine Quality")
 
@@ -9,6 +10,18 @@ try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.initiate_data_validation()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
